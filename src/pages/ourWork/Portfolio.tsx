cff88fb799ad0f305ceb10f37acb40e78b65f2a4
Anurag Bhatt <anuragbhatt1805@gmail.com>
2024-04-12 14:42:36 +0530

    product development phase completed successfully,

--- src/pages/ourWork/Portfolio.tsx
+++ src/pages/ourWork/Portfolio.tsx
@@ -38,7 +38,7 @@ function Portfolio() {
         {portfolios?.map((portfolio, index) => {
           return (
             <section key={index} className="rounded-3xl mt-10 h-[70vh] border-4 p-2 grid grid-cols-[60%_40%] gap-3 shadow-xl drop-shadow-xl">
-              <div className="overflow-y-hidden my-4">
+              <div className="overflow-y-hidden my-4 ml-6">
                 {
                   portfolio.pdf && (
                     // <PortfolioPdf pdfURL={"https://clickdimensions.com/links/TestPDFfile.pdf"} />
--- src/pages/ourWork/PortfolioPdf.tsx
+++ src/pages/ourWork/PortfolioPdf.tsx
@@ -13,19 +13,28 @@ function PortfolioPdf({ pdfURL }: { pdfURL: string }) {
   const [numPage, setNumPage] = useState<number>(0);
   const [pageNumber, setPageNumber] = useState<number>(1);
 
-  const [pdfData, setPdfData] = useState<any>(null);
+//   const [pdfData, setPdfData] = useState<any>(null);
 
-  useEffect(() => {
-    const fetchData = async () => {
-        console.log(pdfURL);
-      const response = await fetch(pdfURL, {mode:"no-cors"});
-      console.log(response);
-      const data = await response.blob(); // Get the binary data
-      setPdfData(data);
-    };
+//   useEffect(() => {
+//     const fetchData = async () => {
+//         console.log(pdfURL);
+//       const response = await fetch(pdfURL, {
+//         headers:{
+//             "Content-Type": "application/pdf",
+//             "Accept": "application/pdf",
+//             "Access-Control-Allow-Origin": "*",
+//             "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+//             "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization",
+//             "Access-Control-Max-Age": "0"
+//         }
+//       });
+//       console.log(response);
+//       const data = await response.blob(); // Get the binary data
+//       setPdfData(data);
+//     };
 
-    fetchData();
-  }, [pdfURL]);
+//     fetchData();
+//   }, [pdfURL]);
 
 
   function onDocumentLoadSuccess({ numPages }: { numPages: number }) {
@@ -52,22 +61,21 @@ function PortfolioPdf({ pdfURL }: { pdfURL: string }) {
 
   return (
     <>
-          <div className=" rounded-full p-2 bg-black/20 text-white cursor-pointer h-fit z-10 w-fit absolute left-5 top-[50%]">
+          <div className=" rounded-full p-2 bg-black/20 text-white cursor-pointer h-fit z-10 w-fit absolute left-10 top-[50%]">
             <BsChevronCompactLeft onClick={setPrevPage} size={30} />
           </div>
-          <div className=" rounded-full p-2 bg-black/20 text-white cursor-pointer h-fit z-10 w-fit absolute right-[41%] top-[50%]">
+          <div className=" rounded-full p-2 bg-black/20 text-white cursor-pointer h-fit z-10 w-fit absolute right-[40%] top-[50%]">
             <BsChevronCompactRight onClick={setNextPage} size={30} />
         </div>
       <div className="my-4 w-full pb-5 flex flex-row flex-wrap justify-center items-center overflow-y-hidden">
         <div className="flex flex-row w-full justify-center">
 
           <Document
-            file={pdfData}
+            file={pdfURL}
             onLoadSuccess={onDocumentLoadSuccess}
           >
             <Page pageNumber={pageNumber} />
           </Document>
-          {/* <embed src={pdfURL} type="application/pdf" width="200%" height="100%" /> */}
 
         </div>
       </div>
