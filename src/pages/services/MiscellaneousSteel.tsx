7a6953105155debb17254b0b09dddd3127a7eafd
Raunak <raunakdob@gmail.com>
2025-09-09 17:45:06 +0530

    refactor: Enhance ImageModal with automatic image sliding and animation effects; update layout in MiscellaneousSteel and StructuralSteel pages for better responsiveness

--- src/pages/services/MiscellaneousSteel.tsx
+++ src/pages/services/MiscellaneousSteel.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect } from "react";
 import { PageBanner, BannerPropType } from "../../components/banner";
 import Newsletter from "../../components/newsletter/Newsletter";
 import Estimate from "../../components/estimation/Estimate";
@@ -10,13 +10,13 @@ const banner: BannerPropType = {
     "https://res.cloudinary.com/dp7yxzrgw/image/upload/v1753685618/banner-image/misc-banner_x3hbow.jpg",
 };
 
-const headSection:string[] = [
+const headSection: string[] = [
   "Our portfolio spans across structural steel elements such as complex staircases, hand-railings and other intricate misc. steel construction items. We understand the detailing needs from a fabrication point of view and execute comprehensive drawings that are conformed to your unique requirements.",
   "Using the latest 3D modeling tools and in compliance with the AISC code, we will make sure you present your bids on time and in entirety.",
-  "No job is too small for us and nothing is miscellaneous when you add an element of technical clarity to it."
-]
+  "No job is too small for us and nothing is miscellaneous when you add an element of technical clarity to it.",
+];
 
-const MiscCapability:string[] = [
+const MiscCapability: string[] = [
   "Egress Stairs (Switch back/Box stair/Scissor stairs) with Railing",
   "Ship ladder",
   "Industrial access stairs with railing",
@@ -27,52 +27,36 @@ const MiscCapability:string[] = [
   "Dumpster gate",
   "Ladder with / without gage",
   "Embeds",
-]
+];
 
 const MiscType = [
   {
     title: "Types of Railing patterns",
-    description: [
-      "Picket Type Railing",
-      "Inline Rail System",
-      "Mesh Type",
-    ]
+    description: ["Picket Type Railing", "Inline Rail System", "Mesh Type"],
   },
   {
     title: "Types of Stringer Material",
-    description: [
-      "Channel",
-      "Plate",
-      "Built-up tube",
-      "Rolled tube"
-    ]
+    description: ["Channel", "Plate", "Built-up tube", "Rolled tube"],
   },
   {
     title: "Types of Treads",
-    description: [
-      "Concrete pan",
-      "Checkered plate",
-      "Tuff tread",
-      "Tuff coat"
-    ]
+    description: ["Concrete pan", "Checkered plate", "Tuff tread", "Tuff coat"],
   },
   {
     title: "Types of Codes",
-    description: [
-      "AISC", "IBC", "UBC", "OSHA", "BOCA, etc"
-    ]
-  }
-]
+    description: ["AISC", "IBC", "UBC", "OSHA", "BOCA, etc"],
+  },
+];
 
 function MiscellaneousSteel() {
-  useEffect(()=>{
-    document.title = "Miscellaneous Steel Detailing"
-  })
+  useEffect(() => {
+    document.title = "Miscellaneous Steel Detailing";
+  });
 
   return (
     <>
       <PageBanner {...banner} />
-      <section className="m-10 flex flex-wrap justify-start items-start mx-auto lg:max-w-screen-lg xl:max-w-screen-xl">
+      <section className=" flex flex-wrap justify-center items-start mx-auto ">
         <div className="flex flex-wrap flex-col">
           <div className="mx-auto my-0 m-28 lg:max-w-screen-lg xl:max-w-screen-xl">
             <section className="rounded-3xl mt-3 border-2 p-2 grid grid-cols-[60%_40%] gap-3 shadow-md max-md:grid-cols-1">
@@ -96,7 +80,7 @@ function MiscellaneousSteel() {
             </section>
           </div>
 
-          <div className="bg-gray-100 my-5 rounded-2xl">
+          <div className="bg-gray-100 my-5 rounded-2xl w-screen">
             <div className="p-5 mx-auto my-10 lg:max-w-screen-lg xl:max-w-screen-xl pb-7">
               <div className="text-3xl font-semibold  text-[#6abd45] px-3">
                 Miscellaneous Detailing team Capabilities
@@ -135,48 +119,47 @@ function MiscellaneousSteel() {
               </section>
             </div>
           </div>
-
-          <div className="mx-auto w-full ">
-            <section className="grid grid-cols-1 gap-2 p-2 mt-3 border-2 shadow-md  rounded-2xl md:grid-cols-4 lg:grid-cols-4 px-4">
-              {MiscType?.map((type, index) => {
-                return (
-                  <div key={index} className="m-4 leading-loose text-gray-700">
-                    <div className="text-xl font-semibold my-2 text-[#6abd45]">
-                      {type.title}
-                    </div>
-                    {type.description?.map((detail, index) => {
-                      return (
-                        <div
-                          key={index}
-                          className="flex flex-row flex-wrap items-center py-1 md:py-2"
-                        >
-                          <svg
-                            className="h-6 w-6 text-[#6abd45]"
-                            width="24"
-                            height="24"
-                            viewBox="0 0 24 24"
-                            strokeWidth="2"
-                            stroke="currentColor"
-                            fill="none"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                          >
-                            {" "}
-                            <path stroke="none" d="M0 0h24v24H0z" />{" "}
-                            <path
-                              d="M18 15l-6-6l-6 6h12"
-                              transform="rotate(90 12 12)"
-                            />
-                          </svg>
-                          <p className="flex text-lg">{detail}</p>
-                        </div>
-                      );
-                    })}
+        </div>
+        <div className="mx-auto w-screen lg:max-w-screen-lg xl:max-w-screen-xl">
+          <section className="grid grid-cols-1 gap-2 p-2 mt-3 border-2 shadow-md  rounded-2xl md:grid-cols-4 lg:grid-cols-4 px-4">
+            {MiscType?.map((type, index) => {
+              return (
+                <div key={index} className="m-4 leading-loose text-gray-700">
+                  <div className="text-xl font-semibold my-2 text-[#6abd45]">
+                    {type.title}
                   </div>
-                );
-              })}
-            </section>
-          </div>
+                  {type.description?.map((detail, index) => {
+                    return (
+                      <div
+                        key={index}
+                        className="flex flex-row flex-wrap items-center py-1 md:py-2"
+                      >
+                        <svg
+                          className="h-6 w-6 text-[#6abd45]"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                          strokeWidth="2"
+                          stroke="currentColor"
+                          fill="none"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        >
+                          {" "}
+                          <path stroke="none" d="M0 0h24v24H0z" />{" "}
+                          <path
+                            d="M18 15l-6-6l-6 6h12"
+                            transform="rotate(90 12 12)"
+                          />
+                        </svg>
+                        <p className="flex text-lg">{detail}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+              );
+            })}
+          </section>
         </div>
       </section>
       <Newsletter />
@@ -184,4 +167,4 @@ function MiscellaneousSteel() {
   );
 }
 
-export default MiscellaneousSteel
+export default MiscellaneousSteel;
