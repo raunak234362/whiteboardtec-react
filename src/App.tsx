e5de0643f724b87bf09094125d47746b167629bb
Siddhi Singh Rathor <siingh.siddhi@gmail.com>
2026-08-27 15:50:15 +0530

    fix: correct notification toggle logic and update layout to use flexbox for sticky footer implementation

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,29 @@ import { useLocation } from "react-router-dom";
 // import GoTo from "./components/goto/GoTo";
 
 function App(): JSX.Element {
-
-
   const location = useLocation();
   if (location.pathname.startsWith("/admin")) {
     return (
-      <>
-        <Outlet />
+      <div className="min-h-screen flex flex-col justify-between">
+        <div className="flex-1">
+          <Outlet />
+        </div>
         <Footer />
-      </>
+      </div>
     );
   } else {
     console.log("App component rendered with location:", location.pathname);
     return (
-      <>
-
-        <div className="max-md:flex max-md:flex-wrap max-md:justify-between">
-          {/* {(location.pathname === '/') ? <HeaderHome /> : <HeaderBase />} */}
-          <HeaderHome />
-          {location.pathname === "/" ? <HomeNav /> : <NavigationBar />}
+      <div className="min-h-screen flex flex-col justify-between">
+        <div className="flex-1">
+          <div className="max-md:flex max-md:flex-wrap max-md:justify-between">
+            <HeaderHome />
+            {location.pathname === "/" ? <HomeNav /> : <NavigationBar />}
+          </div>
+          <Outlet />
         </div>
-        <Outlet />
-        {/* {location.pathname !== "/" && <GoTo props={reference} />} */}
         <Footer />
-      </>
+      </div>
     );
   }
 }
